d0043cdddcb775e804285e695d284a7de2f85b7a
cecobask <32335835+cecobask@users.noreply.github.com>
2018-02-13 17:11:07 +0000

    Introduced Base class. Refactored Report to display real donations data. Menus are now working with xml driven event handling.

--- app/src/main/java/ie/app/activities/Donate.java
+++ app/src/main/java/ie/app/activities/Donate.java
@@ -4,12 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
@@ -18,8 +15,9 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 import ie.app.R;
+import ie.app.models.Donation;
 
-public class Donate extends AppCompatActivity {
+public class Donate extends Base {
 
     private Button donateButton;
     private RadioGroup paymentMethod;
@@ -64,22 +62,6 @@ public class Donate extends AppCompatActivity {
         progressBar.setMax(10000);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_donate, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId())
-        {
-            case R.id.menuReport : startActivity (new Intent(this, Report.class));
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     public void donateButtonPressed(View view){
         int amount = amountPicker.getValue();
@@ -101,11 +83,13 @@ public class Donate extends AppCompatActivity {
         }
 
         if(!targetArchieved){
+            newDonation(new Donation(amount, method));
             totalDonated+=amount;
             progressBar.setProgress(totalDonated);
             targetArchieved = totalDonated > 10000;
             String totalStr = "$" + totalDonated;
             total.setText(totalStr);
+            Toast.makeText(this, "You donated amount:" + amount + ", Method: " + method, Toast.LENGTH_SHORT).show();
         }
         else{
             Toast.makeText(this, "Target Exceeded!", Toast.LENGTH_SHORT).show();
--- app/src/main/java/ie/app/activities/Report.java
+++ app/src/main/java/ie/app/activities/Report.java
@@ -1,26 +1,16 @@
 package ie.app.activities;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 import ie.app.R;
 
 
-public class Report extends AppCompatActivity
+public class Report extends Base
 {
     ListView listView;
-
-    static final String[] numbers = new String[] {
-            "Amount, Pay method",
-            "10,     Direct",
-            "100,    PayPal",
-            "1000,   Direct",
-            "10,     PayPal",
-            "5000,   PayPal"};
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -29,25 +19,15 @@ public class Report extends AppCompatActivity
         setContentView(R.layout.activity_report);
 
         listView = (ListView) findViewById(R.id.reportList);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,  android.R.layout.simple_list_item_1, numbers);
+        DonationAdapter adapter = new DonationAdapter(this,  donations);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Report.this,"You Selected Row [" + position + "] For Donation Data "
+                        + donations.get(position),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_donate, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
-            case R.id.menuDonate : startActivity (new Intent(this, Donate.class));
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
